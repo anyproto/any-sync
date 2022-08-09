ff5a0d6a45724e3719fe331714bede66f3c55a25
mcrakhman <rakhmanov.m@gmail.com>
2022-08-09 21:48:14 +0200

    Start refactoring tree package

---
+++ pkg/acl/tree/tree.go
@@ -0,0 +1,416 @@
+package tree
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"github.com/anytypeio/go-anytype-infrastructure-experiments/util/slice"
+	"sort"
+)
+
+type Mode int
+
+const (
+	Append Mode = iota
+	Rebuild
+	Nothing
+)
+
+// TODO: consider abstracting into separate package with iterator, remove
+type Tree struct {
+	root        *Change
+	headIds     []string
+	metaHeadIds []string
+	attached    map[string]*Change
+	unAttached  map[string]*Change
+	// missed id -> list of dependency ids
+	waitList       map[string][]string
+	invalidChanges map[string]struct{}
+
+	// bufs
+	iterCompBuf []*Change
+	iterQueue   []*Change
+
+	duplicateEvents int
+}
+
+func (t *Tree) GetUnattachedChanges(changes ...*Change) []*Change {
+	return nil
+}
+
+func (t *Tree) RootId() string {
+	if t.root != nil {
+		return t.root.Id
+	}
+	return ""
+}
+
+func (t *Tree) Root() *Change {
+	return t.root
+}
+
+func (t *Tree) AddFast(changes ...*Change) {
+	for _, c := range changes {
+		// ignore existing
+		if _, ok := t.attached[c.Id]; ok {
+			continue
+		} else if _, ok := t.unAttached[c.Id]; ok {
+			continue
+		}
+		t.add(c)
+	}
+	t.updateHeads()
+}
+
+func (t *Tree) Add(changes ...*Change) (mode Mode) {
+	var beforeHeadIds = t.headIds
+	var attached bool
+	var empty = t.Len() == 0
+	for _, c := range changes {
+		// ignore existing
+		if _, ok := t.attached[c.Id]; ok {
+			continue
+		} else if _, ok := t.unAttached[c.Id]; ok {
+			continue
+		}
+		if t.add(c) {
+			attached = true
+		}
+	}
+	if !attached {
+		return Nothing
+	}
+	t.updateHeads()
+	if empty {
+		return Rebuild
+	}
+	for _, hid := range beforeHeadIds {
+		for _, newCh := range changes {
+			if _, ok := t.attached[newCh.Id]; ok {
+				if !t.after(newCh.Id, hid) {
+					return Rebuild
+				}
+			}
+		}
+	}
+	return Append
+}
+
+func (t *Tree) RemoveInvalidChange(id string) {
+	stack := []string{id}
+	// removing all children of this id (either next or unattached)
+	for len(stack) > 0 {
+		var exists bool
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if _, exists = t.invalidChanges[top]; exists {
+			continue
+		}
+
+		var rem *Change
+		t.invalidChanges[top] = struct{}{}
+		if rem, exists = t.unAttached[top]; exists {
+			delete(t.unAttached, top)
+		} else if rem, exists = t.attached[top]; exists {
+			// remove from all prev changes
+			for _, id := range rem.PreviousIds {
+				prev, exists := t.attached[id]
+				if !exists {
+					continue
+				}
+				for i, next := range prev.Next {
+					if next.Id == top {
+						prev.Next[i] = nil
+						prev.Next = append(prev.Next[:i], prev.Next[i+1:]...)
+						break
+					}
+				}
+			}
+			delete(t.attached, top)
+		}
+		for _, el := range rem.Unattached {
+			stack = append(stack, el.Id)
+		}
+		for _, el := range rem.Next {
+			stack = append(stack, el.Id)
+		}
+	}
+	t.updateHeads()
+}
+
+func (t *Tree) add(c *Change) (attached bool) {
+	if c == nil {
+		return false
+	}
+	if _, exists := t.invalidChanges[c.Id]; exists {
+		return false
+	}
+
+	if t.root == nil { // first element
+		t.root = c
+		t.attached = map[string]*Change{
+			c.Id: c,
+		}
+		t.unAttached = make(map[string]*Change)
+		t.waitList = make(map[string][]string)
+		t.invalidChanges = make(map[string]struct{})
+		return true
+	}
+	if len(c.PreviousIds) > 1 {
+		sort.Strings(c.PreviousIds)
+	}
+	// attaching only if all prev ids are attached
+	attached = true
+	for _, pid := range c.PreviousIds {
+		if prev, ok := t.attached[pid]; ok {
+			prev.Unattached = append(prev.Unattached, c)
+			continue
+		}
+		attached = false
+		if prev, ok := t.unAttached[pid]; ok {
+			prev.Unattached = append(prev.Unattached, c)
+			continue
+		}
+		wl := t.waitList[pid]
+		wl = append(wl, c.Id)
+		t.waitList[pid] = wl
+	}
+	if attached {
+		t.attach(c, true)
+	} else {
+		// clearing wait list
+		for _, wid := range t.waitList[c.Id] {
+			c.Unattached = append(c.Unattached, t.unAttached[wid])
+		}
+		delete(t.waitList, c.Id)
+		t.unAttached[c.Id] = c
+	}
+	return
+}
+
+func (t *Tree) canAttach(c *Change) (attach bool) {
+	if c == nil {
+		return false
+	}
+	attach = true
+	for _, id := range c.PreviousIds {
+		if _, exists := t.attached[id]; !exists {
+			attach = false
+		}
+	}
+	return
+}
+
+func (t *Tree) attach(c *Change, newEl bool) {
+	t.attached[c.Id] = c
+	if !newEl {
+		delete(t.unAttached, c.Id)
+	}
+
+	// add next to all prev changes
+	for _, id := range c.PreviousIds {
+		// prev id must be attached if we attach this id
+		prev := t.attached[id]
+		prev.Next = append(prev.Next, c)
+		if len(prev.Next) > 1 {
+			sort.Sort(sortChanges(prev.Next))
+		}
+		for i, next := range prev.Unattached {
+			if next.Id == c.Id {
+				prev.Unattached[i] = nil
+				prev.Unattached = append(prev.Unattached[:i], prev.Unattached[i+1:]...)
+				break
+			}
+		}
+	}
+
+	// clearing wait list
+	if waitIds, ok := t.waitList[c.Id]; ok {
+		for _, wid := range waitIds {
+			next := t.unAttached[wid]
+			if t.canAttach(next) {
+				t.attach(next, false)
+			}
+		}
+		delete(t.waitList, c.Id)
+	}
+
+	for _, next := range c.Unattached {
+		if t.canAttach(next) {
+			t.attach(next, false)
+		}
+	}
+}
+
+func (t *Tree) after(id1, id2 string) (found bool) {
+	t.iterate(t.attached[id2], func(c *Change) (isContinue bool) {
+		if c.Id == id1 {
+			found = true
+			return false
+		}
+		return true
+	})
+	return
+}
+
+func (t *Tree) dfs(startChange string) (uniqMap map[string]*Change) {
+	stack := make([]*Change, 0, 10)
+	stack = append(stack, t.attached[startChange])
+	uniqMap = map[string]*Change{}
+
+	for len(stack) > 0 {
+		ch := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if _, exists := uniqMap[ch.Id]; exists {
+			continue
+		}
+
+		uniqMap[ch.Id] = ch
+
+		for _, prev := range ch.PreviousIds {
+			stack = append(stack, t.attached[prev])
+		}
+	}
+	return uniqMap
+}
+
+func (t *Tree) updateHeads() {
+	var newHeadIds, newMetaHeadIds []string
+	t.iterate(t.root, func(c *Change) (isContinue bool) {
+		if len(c.Next) == 0 {
+			newHeadIds = append(newHeadIds, c.Id)
+		}
+		return true
+	})
+	t.headIds = newHeadIds
+	t.metaHeadIds = newMetaHeadIds
+	sort.Strings(t.headIds)
+	sort.Strings(t.metaHeadIds)
+}
+
+func (t *Tree) ACLHeads() []string {
+	var aclTreeHeads []string
+	for _, head := range t.Heads() {
+		if slice.FindPos(aclTreeHeads, head) != -1 { // do not scan known heads
+			continue
+		}
+		precedingHeads := t.getPrecedingACLHeads(head)
+
+		for _, aclHead := range precedingHeads {
+			if slice.FindPos(aclTreeHeads, aclHead) != -1 {
+				continue
+			}
+			aclTreeHeads = append(aclTreeHeads, aclHead)
+		}
+	}
+	return aclTreeHeads
+}
+
+func (t *Tree) getPrecedingACLHeads(head string) []string {
+	headChange := t.attached[head]
+
+	if headChange.Content.GetAclData() != nil {
+		return []string{head}
+	} else {
+		return headChange.Content.AclHeadIds
+	}
+}
+
+func (t *Tree) iterate(start *Change, f func(c *Change) (isContinue bool)) {
+	it := newIterator()
+	defer freeIterator(it)
+	it.iterate(start, f)
+}
+
+func (t *Tree) iterateSkip(start *Change, skipBefore *Change, f func(c *Change) (isContinue bool)) {
+	it := newIterator()
+	defer freeIterator(it)
+	it.iterateSkip(start, skipBefore, f)
+}
+
+func (t *Tree) IterateSkip(startId string, skipBeforeId string, f func(c *Change) (isContinue bool)) {
+	it := newIterator()
+	defer freeIterator(it)
+	it.iterateSkip(t.attached[startId], t.attached[skipBeforeId], f)
+}
+
+func (t *Tree) Iterate(startId string, f func(c *Change) (isContinue bool)) {
+	t.iterate(t.attached[startId], f)
+}
+
+func (t *Tree) IterateBranching(startId string, f func(c *Change, branchLevel int) (isContinue bool)) {
+	// branchLevel indicates the number of parallel branches
+	var bc int
+	t.iterate(t.attached[startId], func(c *Change) (isContinue bool) {
+		if pl := len(c.PreviousIds); pl > 1 {
+			bc -= pl - 1
+		}
+		bl := bc
+		if nl := len(c.Next); nl > 1 {
+			bc += nl - 1
+		}
+		return f(c, bl)
+	})
+}
+
+func (t *Tree) Hash() string {
+	h := md5.New()
+	n := 0
+	t.iterate(t.root, func(c *Change) (isContinue bool) {
+		n++
+		fmt.Fprintf(h, "-%s", c.Id)
+		return true
+	})
+	return fmt.Sprintf("%d-%x", n, h.Sum(nil))
+}
+
+func (t *Tree) GetDuplicateEvents() int {
+	return t.duplicateEvents
+}
+
+func (t *Tree) ResetDuplicateEvents() {
+	t.duplicateEvents = 0
+}
+
+func (t *Tree) Len() int {
+	return len(t.attached)
+}
+
+func (t *Tree) Heads() []string {
+	return t.headIds
+}
+
+func (t *Tree) String() string {
+	var buf = bytes.NewBuffer(nil)
+	t.Iterate(t.RootId(), func(c *Change) (isContinue bool) {
+		buf.WriteString(c.Id)
+		if len(c.Next) > 1 {
+			buf.WriteString("-<")
+		} else if len(c.Next) > 0 {
+			buf.WriteString("->")
+		} else {
+			buf.WriteString("-|")
+		}
+		return true
+	})
+	return buf.String()
+}
+
+func (t *Tree) Get(id string) *Change {
+	return t.attached[id]
+}
+
+type sortChanges []*Change
+
+func (s sortChanges) Len() int {
+	return len(s)
+}
+
+func (s sortChanges) Less(i, j int) bool {
+	return s[i].Id < s[j].Id
+}
+
+func (s sortChanges) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
---
+++ pkg/acl/tree/treebuilder.go
@@ -0,0 +1,365 @@
+package tree
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/anytypeio/go-anytype-infrastructure-experiments/app/logger"
+	"github.com/anytypeio/go-anytype-infrastructure-experiments/pkg/acl/aclchanges/aclpb"
+	"github.com/anytypeio/go-anytype-infrastructure-experiments/pkg/acl/treestorage"
+	"github.com/anytypeio/go-anytype-infrastructure-experiments/util/keys/asymmetric/signingkey"
+	"github.com/anytypeio/go-anytype-infrastructure-experiments/util/slice"
+	"github.com/gogo/protobuf/proto"
+	"time"
+)
+
+var (
+	log      = logger.NewNamed("acltree").Sugar()
+	ErrEmpty = errors.New("logs empty")
+)
+
+type treeBuilder struct {
+	cache                map[string]*Change
+	identityKeys         map[string]signingkey.PubKey
+	signingPubKeyDecoder signingkey.PubKeyDecoder
+	tree                 *Tree
+	treeStorage          treestorage.TreeStorage
+}
+
+func newTreeBuilder(t treestorage.TreeStorage, decoder signingkey.PubKeyDecoder) *treeBuilder {
+	return &treeBuilder{
+		signingPubKeyDecoder: decoder,
+		treeStorage:          t,
+	}
+}
+
+func (tb *treeBuilder) Init() {
+	tb.cache = make(map[string]*Change)
+	tb.identityKeys = make(map[string]signingkey.PubKey)
+	tb.tree = &Tree{}
+}
+
+func (tb *treeBuilder) Build(fromStart bool) (*Tree, error) {
+	var headsAndOrphans []string
+	orphans, err := tb.treeStorage.Orphans()
+	if err != nil {
+		return nil, err
+	}
+	heads, err := tb.treeStorage.Heads()
+	if err != nil {
+		return nil, err
+	}
+	headsAndOrphans = append(headsAndOrphans, orphans...)
+	headsAndOrphans = append(headsAndOrphans, heads...)
+
+	if fromStart {
+		if err := tb.buildTreeFromStart(headsAndOrphans); err != nil {
+			return nil, fmt.Errorf("buildTree error: %v", err)
+		}
+	} else {
+		breakpoint, err := tb.findBreakpoint(headsAndOrphans)
+		if err != nil {
+			return nil, fmt.Errorf("findBreakpoint error: %v", err)
+		}
+
+		if err = tb.buildTree(headsAndOrphans, breakpoint); err != nil {
+			return nil, fmt.Errorf("buildTree error: %v", err)
+		}
+	}
+
+	tb.cache = nil
+
+	return tb.tree, nil
+}
+
+func (tb *treeBuilder) buildTreeFromStart(heads []string) (err error) {
+	changes, root, err := tb.dfsFromStart(heads)
+	if err != nil {
+		return err
+	}
+
+	tb.tree.AddFast(root)
+	tb.tree.AddFast(changes...)
+	return
+}
+
+func (tb *treeBuilder) dfsFromStart(heads []string) (buf []*Change, root *Change, err error) {
+	var possibleRoots []*Change
+	stack := make([]string, len(heads), len(heads)*2)
+	copy(stack, heads)
+
+	buf = make([]*Change, 0, len(stack)*2)
+	uniqMap := make(map[string]struct{})
+	for len(stack) > 0 {
+		id := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if _, exists := uniqMap[id]; exists {
+			continue
+		}
+
+		ch, err := tb.loadChange(id)
+		if err != nil {
+			continue
+		}
+
+		uniqMap[id] = struct{}{}
+		buf = append(buf, ch)
+
+		for _, prev := range ch.PreviousIds {
+			stack = append(stack, prev)
+		}
+		if len(ch.PreviousIds) == 0 {
+			possibleRoots = append(possibleRoots, ch)
+		}
+	}
+	header, err := tb.treeStorage.Header()
+	if err != nil {
+		return nil, nil, err
+	}
+	for _, r := range possibleRoots {
+		if r.Id == header.FirstChangeId {
+			return buf, r, nil
+		}
+	}
+
+	return nil, nil, fmt.Errorf("could not find root change")
+}
+
+func (tb *treeBuilder) buildTree(heads []string, breakpoint string) (err error) {
+	ch, err := tb.loadChange(breakpoint)
+	if err != nil {
+		return
+	}
+	tb.tree.AddFast(ch)
+	changes, err := tb.dfs(heads, breakpoint, tb.loadChange)
+
+	tb.tree.AddFast(changes...)
+	return
+}
+
+func (tb *treeBuilder) dfs(
+	heads []string,
+	breakpoint string,
+	load func(string) (*Change, error)) (buf []*Change, err error) {
+	stack := make([]string, len(heads), len(heads)*2)
+	copy(stack, heads)
+
+	buf = make([]*Change, 0, len(stack)*2)
+	uniqMap := map[string]struct{}{breakpoint: {}}
+	for len(stack) > 0 {
+		id := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if _, exists := uniqMap[id]; exists {
+			continue
+		}
+
+		ch, err := load(id)
+		if err != nil {
+			continue
+		}
+
+		uniqMap[id] = struct{}{}
+		buf = append(buf, ch)
+
+		for _, prev := range ch.PreviousIds {
+			stack = append(stack, prev)
+		}
+	}
+	return buf, nil
+}
+
+func (tb *treeBuilder) loadChange(id string) (ch *Change, err error) {
+	if ch, ok := tb.cache[id]; ok {
+		return ch, nil
+	}
+
+	// TODO: Add virtual changes logic
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	change, err := tb.treeStorage.GetChange(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	verifiedChange, err := tb.makeVerifiedChange(change)
+	if err != nil {
+		return nil, err
+	}
+
+	tb.cache[id] = NewChange(id, verifiedChange)
+
+	return ch, nil
+}
+
+func (tb *treeBuilder) verify(identity string, payload, signature []byte) (isVerified bool, err error) {
+	identityKey, exists := tb.identityKeys[identity]
+	if !exists {
+		identityKey, err = tb.signingPubKeyDecoder.DecodeFromString(identity)
+		if err != nil {
+			return
+		}
+		tb.identityKeys[identity] = identityKey
+	}
+	return identityKey.Verify(payload, signature)
+}
+
+func (tb *treeBuilder) makeVerifiedChange(change *aclpb.RawChange) (aclChange *aclpb.Change, err error) {
+	aclChange = new(aclpb.Change)
+
+	// TODO: think what should we do with such cases, because this can be used by attacker to break our Tree
+	if err = proto.Unmarshal(change.Payload, aclChange); err != nil {
+		return
+	}
+	var verified bool
+	verified, err = tb.verify(aclChange.Identity, change.Payload, change.Signature)
+	if err != nil {
+		return
+	}
+	if !verified {
+		err = fmt.Errorf("the signature of the payload cannot be verified")
+		return
+	}
+	return
+}
+
+func (tb *treeBuilder) makeUnverifiedACLChange(change *aclpb.RawChange) (aclChange *aclpb.ACLChange, err error) {
+	aclChange = new(aclpb.ACLChange)
+	err = proto.Unmarshal(change.Payload, aclChange)
+	return
+}
+
+func (tb *treeBuilder) findBreakpoint(heads []string) (breakpoint string, err error) {
+	var (
+		ch          *Change
+		snapshotIds []string
+	)
+	for _, head := range heads {
+		if ch, err = tb.loadChange(head); err != nil {
+			return
+		}
+		shId := ch.SnapshotId
+		if ch.IsSnapshot {
+			shId = ch.Id
+		}
+		if slice.FindPos(snapshotIds, shId) == -1 {
+			snapshotIds = append(snapshotIds, shId)
+		}
+	}
+	return tb.findCommonSnapshot(snapshotIds)
+}
+
+func (tb *treeBuilder) findCommonSnapshot(snapshotIds []string) (snapshotId string, err error) {
+	if len(snapshotIds) == 1 {
+		return snapshotIds[0], nil
+	} else if len(snapshotIds) == 0 {
+		return "", fmt.Errorf("snapshots not found")
+	}
+
+	for len(snapshotIds) > 1 {
+		l := len(snapshotIds)
+		shId, e := tb.findCommonForTwoSnapshots(snapshotIds[l-2], snapshotIds[l-1])
+		if e != nil {
+			return "", e
+		}
+		snapshotIds[l-2] = shId
+		snapshotIds = snapshotIds[:l-1]
+	}
+	return snapshotIds[0], nil
+}
+
+func (tb *treeBuilder) findCommonForTwoSnapshots(s1, s2 string) (s string, err error) {
+	// fast cases
+	if s1 == s2 {
+		return s1, nil
+	}
+	ch1, err := tb.loadChange(s1)
+	if err != nil {
+		return "", err
+	}
+	if ch1.SnapshotId == s2 {
+		return s2, nil
+	}
+	ch2, err := tb.loadChange(s2)
+	if err != nil {
+		return "", err
+	}
+	if ch2.SnapshotId == s1 {
+		return s1, nil
+	}
+	if ch1.SnapshotId == ch2.SnapshotId && ch1.SnapshotId != "" {
+		return ch1.SnapshotId, nil
+	}
+	// traverse
+	var t1 = make([]string, 0, 5)
+	var t2 = make([]string, 0, 5)
+	t1 = append(t1, ch1.Id, ch1.SnapshotId)
+	t2 = append(t2, ch2.Id, ch2.SnapshotId)
+	for {
+		lid1 := t1[len(t1)-1]
+		if lid1 != "" {
+			l1, e := tb.loadChange(lid1)
+			if e != nil {
+				return "", e
+			}
+			if l1.SnapshotId != "" {
+				if slice.FindPos(t2, l1.SnapshotId) != -1 {
+					return l1.SnapshotId, nil
+				}
+			}
+			t1 = append(t1, l1.SnapshotId)
+		}
+		lid2 := t2[len(t2)-1]
+		if lid2 != "" {
+			l2, e := tb.loadChange(t2[len(t2)-1])
+			if e != nil {
+				return "", e
+			}
+			if l2.SnapshotId != "" {
+				if slice.FindPos(t1, l2.SnapshotId) != -1 {
+					return l2.SnapshotId, nil
+				}
+			}
+			t2 = append(t2, l2.SnapshotId)
+		}
+		if lid1 == "" && lid2 == "" {
+			break
+		}
+	}
+
+	log.Warnf("changes build Tree: possible versions split")
+
+	// prefer not first snapshot
+	if len(ch1.PreviousIds) == 0 && len(ch2.PreviousIds) > 0 {
+		log.Warnf("changes build Tree: prefer %s(%d prevIds) over %s(%d prevIds)", s2, len(ch2.PreviousIds), s1, len(ch1.PreviousIds))
+		return s2, nil
+	} else if len(ch1.PreviousIds) > 0 && len(ch2.PreviousIds) == 0 {
+		log.Warnf("changes build Tree: prefer %s(%d prevIds) over %s(%d prevIds)", s1, len(ch1.PreviousIds), s2, len(ch2.PreviousIds))
+		return s1, nil
+	}
+
+	isEmptySnapshot := func(ch *Change) bool {
+		// TODO: add more sophisticated checks in Change for snapshots
+		return !ch.IsSnapshot
+	}
+
+	// TODO: can we even have empty snapshots?
+	// prefer not empty snapshot
+	if isEmptySnapshot(ch1) && !isEmptySnapshot(ch2) {
+		log.Warnf("changes build Tree: prefer %s(not empty) over %s(empty)", s2, s1)
+		return s2, nil
+	} else if isEmptySnapshot(ch2) && !isEmptySnapshot(ch1) {
+		log.Warnf("changes build Tree: prefer %s(not empty) over %s(empty)", s1, s2)
+		return s1, nil
+	}
+
+	// TODO: add virtual change mechanics
+	// unexpected behavior - just return lesser id
+	if s1 < s2 {
+		log.Warnf("changes build Tree: prefer %s (%s<%s)", s1, s1, s2)
+		return s1, nil
+	}
+	log.Warnf("changes build Tree: prefer %s (%s<%s)", s2, s2, s1)
+
+	return s2, nil
+}
