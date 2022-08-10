d23cc0a239f4a0313c727b730973085f00ae38e1
mcrakhman <rakhmanov.m@gmail.com>
2022-08-10 11:52:45 +0200

    Add acltree in new package

---
+++ pkg/acl/tree/descriptionparser.go
@@ -0,0 +1,45 @@
+package tree
+
+import (
+	"fmt"
+	"github.com/anytypeio/go-anytype-infrastructure-experiments/pkg/acl/aclchanges/aclpb"
+	"github.com/gogo/protobuf/proto"
+	"strings"
+	"unicode"
+)
+
+type DescriptionParser interface {
+	ParseChange(*Change) ([]string, error)
+}
+
+var ACLDescriptionParser = aclDescriptionParser{}
+
+type aclDescriptionParser struct{}
+
+func (a aclDescriptionParser) ParseChange(changeWrapper *Change) (res []string, err error) {
+	change := changeWrapper.Content
+	aclData := &aclpb.ACLChangeACLData{}
+
+	if changeWrapper.DecryptedModel != nil {
+		aclData = changeWrapper.DecryptedModel.(*aclpb.ACLChangeACLData)
+	} else {
+		err = proto.Unmarshal(change.ChangesData, aclData)
+		if err != nil {
+			return
+		}
+	}
+
+	var chSymbs []string
+	for _, chc := range aclData.AclContent {
+		tp := fmt.Sprintf("%T", chc.Value)
+		tp = strings.Replace(tp, "ACLChangeACLContentValueValueOf", "", 1)
+		res := ""
+		for _, ts := range tp {
+			if unicode.IsUpper(ts) {
+				res += string(ts)
+			}
+		}
+		chSymbs = append(chSymbs, res)
+	}
+	return chSymbs, nil
+}
