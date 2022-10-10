8ba457bd120415636a961f7f3862e1a33616e625
mcrakhman <rakhmanov.m@gmail.com>
2022-10-10 20:10:47 +0200

    Add some raw prototypes for storage

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/anytypeio/go-anytype-infrastructure-experiments
 go 1.18
 
 require (
+	github.com/akrylysov/pogreb v0.10.1
 	github.com/anytypeio/go-chash v0.0.0-20220629194632-4ad1154fe232
 	github.com/awalterschulze/gographviz v0.0.0-20190522210029-fa59802746ab
 	github.com/cespare/xxhash v1.1.0
@@ -61,12 +62,9 @@ require (
 	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/image v0.0.0-20200119044424-58c23975cae1 // indirect
-	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.1.5 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	lukechampine.com/blake3 v1.1.6 // indirect
---
+++ node/storage/spacestorage.go
@@ -0,0 +1,136 @@
+package storage
+
+import (
+	"github.com/akrylysov/pogreb"
+	"github.com/anytypeio/go-anytype-infrastructure-experiments/common/commonspace/spacesyncproto"
+	spacestorage "github.com/anytypeio/go-anytype-infrastructure-experiments/common/commonspace/storage"
+	"github.com/anytypeio/go-anytype-infrastructure-experiments/pkg/acl/storage"
+	"github.com/gogo/protobuf/proto"
+	"path"
+	"sync"
+)
+
+type spaceStorage struct {
+	objDb *pogreb.DB
+	keys  spaceKeys
+	mx    sync.Mutex
+}
+
+func newSpaceStorage(rootPath string, spaceId string) (store spacestorage.SpaceStorage, err error) {
+	dbPath := path.Join(rootPath, spaceId)
+	objDb, err := pogreb.Open(dbPath, nil)
+	if err != nil {
+		return
+	}
+	keys := spaceKeys{}
+	has, err := objDb.Has([]byte(keys.HeaderKey()))
+	if err != nil {
+		return
+	}
+	if !has {
+		err = spacestorage.ErrSpaceStorageMissing
+		return
+	}
+
+	has, err = objDb.Has([]byte(keys.ACLKey()))
+	if err != nil {
+		return
+	}
+	if !has {
+		err = spacestorage.ErrSpaceStorageMissing
+		return
+	}
+
+	store = &spaceStorage{
+		objDb: objDb,
+		keys:  keys,
+	}
+	return
+}
+
+func createSpaceStorage(rootPath string, payload spacestorage.SpaceStorageCreatePayload) (store spacestorage.SpaceStorage, err error) {
+	dbPath := path.Join(rootPath, payload.Id)
+	db, err := pogreb.Open(dbPath, nil)
+	if err != nil {
+		return
+	}
+
+	keys := spaceKeys{}
+	has, err := db.Has([]byte(keys.HeaderKey()))
+	if err != nil {
+		return
+	}
+	if has {
+		err = spacestorage.ErrSpaceStorageExists
+		return
+	}
+
+	err = db.Put([]byte(payload.RecWithId.Id), payload.RecWithId.Payload)
+	if err != nil {
+		return
+	}
+
+	marshalled, err := payload.SpaceHeader.Marshal()
+	if err != nil {
+		return
+	}
+	err = db.Put([]byte(payload.Id), marshalled)
+	if err != nil {
+		return
+	}
+	store = &spaceStorage{
+		objDb: db,
+		keys:  keys,
+	}
+	return
+}
+
+func (s *spaceStorage) TreeStorage(id string) (storage.TreeStorage, error) {
+	return newTreeStorage(s.objDb, id)
+}
+
+func (s *spaceStorage) CreateTreeStorage(payload storage.TreeStorageCreatePayload) (ts storage.TreeStorage, err error) {
+	s.mx.Lock()
+	defer s.mx.Unlock()
+	has, err := s.objDb.Has([]byte(payload.TreeId))
+	if err != nil {
+		return
+	}
+	if has {
+		err = spacestorage.ErrSpaceStorageExists
+		return
+	}
+	return createTreeStorage(s.objDb, payload)
+}
+
+func (s *spaceStorage) ACLStorage() (storage.ListStorage, error) {
+	return nil, nil
+}
+
+func (s *spaceStorage) SpaceHeader() (header *spacesyncproto.SpaceHeader, err error) {
+	res, err := s.objDb.Get([]byte(s.keys.HeaderKey()))
+	if err != nil {
+		return
+	}
+
+	header = &spacesyncproto.SpaceHeader{}
+	err = proto.Unmarshal(res, header)
+	return
+}
+
+func (s *spaceStorage) StoredIds() (ids []string, err error) {
+	index := s.objDb.Items()
+	_, value, err := index.Next()
+	for err == nil {
+		strVal := string(value)
+		if isTreeKey(strVal) {
+			ids = append(ids, string(value))
+		}
+		_, value, err = index.Next()
+	}
+	if err != pogreb.ErrIterationDone {
+		return
+	}
+	err = nil
+	return
+}
