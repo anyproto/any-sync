620731b464633adc5da7e3b785eaa985da23350e
mcrakhman <rakhmanov.m@gmail.com>
2022-11-25 14:37:00 +0100

    Re-generate mocks and fix sync tree tests

--- common/commonspace/treegetter/mock_treegetter/mock_treegetter.go
+++ common/commonspace/treegetter/mock_treegetter/mock_treegetter.go
@@ -50,6 +50,20 @@ func (mr *MockTreeGetterMockRecorder) Close(arg0 interface{}) *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Close", reflect.TypeOf((*MockTreeGetter)(nil).Close), arg0)
 }
 
+// DeleteTree mocks base method.
+func (m *MockTreeGetter) DeleteTree(arg0 context.Context, arg1, arg2 string) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "DeleteTree", arg0, arg1, arg2)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// DeleteTree indicates an expected call of DeleteTree.
+func (mr *MockTreeGetterMockRecorder) DeleteTree(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteTree", reflect.TypeOf((*MockTreeGetter)(nil).DeleteTree), arg0, arg1, arg2)
+}
+
 // GetTree mocks base method.
 func (m *MockTreeGetter) GetTree(arg0 context.Context, arg1, arg2 string) (tree.ObjectTree, error) {
 	m.ctrl.T.Helper()
