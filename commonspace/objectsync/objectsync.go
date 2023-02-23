7960297ea283030eedc85b67f66a6ece60aa6984
mcrakhman <rakhmanov.m@gmail.com>
2023-02-23 23:58:09 +0100

    Change deletion logic in headsync and objectsync

--- commonspace/objectsync/objectsync.go
+++ commonspace/objectsync/objectsync.go
@@ -8,7 +8,9 @@ import (
 	"github.com/anytypeio/any-sync/commonspace/objectsync/synchandler"
 	"github.com/anytypeio/any-sync/commonspace/peermanager"
 	"github.com/anytypeio/any-sync/commonspace/spacesyncproto"
+	"github.com/anytypeio/any-sync/nodeconf"
 	"go.uber.org/zap"
+	"golang.org/x/exp/slices"
 	"sync/atomic"
 	"time"
 )
@@ -27,8 +29,9 @@ type ObjectSync interface {
 type objectSync struct {
 	spaceId string
 
-	messagePool  MessagePool
-	objectGetter syncobjectgetter.SyncObjectGetter
+	messagePool   MessagePool
+	objectGetter  syncobjectgetter.SyncObjectGetter
+	configuration nodeconf.Configuration
 
 	syncCtx        context.Context
 	cancelSync     context.CancelFunc
@@ -38,12 +41,14 @@ type objectSync struct {
 func NewObjectSync(
 	spaceId string,
 	spaceIsDeleted *atomic.Bool,
+	configuration nodeconf.Configuration,
 	peerManager peermanager.PeerManager,
 	objectGetter syncobjectgetter.SyncObjectGetter) ObjectSync {
 	syncCtx, cancel := context.WithCancel(context.Background())
 	os := newObjectSync(
 		spaceId,
 		spaceIsDeleted,
+		configuration,
 		objectGetter,
 		syncCtx,
 		cancel)
@@ -55,6 +60,7 @@ func NewObjectSync(
 func newObjectSync(
 	spaceId string,
 	spaceIsDeleted *atomic.Bool,
+	configuration nodeconf.Configuration,
 	objectGetter syncobjectgetter.SyncObjectGetter,
 	syncCtx context.Context,
 	cancel context.CancelFunc,
@@ -65,6 +71,7 @@ func newObjectSync(
 		syncCtx:        syncCtx,
 		cancelSync:     cancel,
 		spaceIsDeleted: spaceIsDeleted,
+		configuration:  configuration,
 	}
 }
 
@@ -85,8 +92,13 @@ func (s *objectSync) HandleMessage(ctx context.Context, senderId string, message
 }
 
 func (s *objectSync) handleMessage(ctx context.Context, senderId string, msg *spacesyncproto.ObjectSyncMessage) (err error) {
+	log := log.With(zap.String("objectId", msg.ObjectId), zap.String("replyId", msg.ReplyId))
 	if s.spaceIsDeleted.Load() {
-		return spacesyncproto.ErrSpaceIsDeleted
+		log = log.With(zap.Bool("isDeleted", true))
+		// preventing sync with other clients
+		if !slices.Contains(s.configuration.NodeIds(s.spaceId), senderId) {
+			return spacesyncproto.ErrSpaceIsDeleted
+		}
 	}
 	log.With(zap.String("objectId", msg.ObjectId), zap.String("replyId", msg.ReplyId)).DebugCtx(ctx, "handling message")
 	obj, err := s.objectGetter.GetObject(ctx, msg.ObjectId)
