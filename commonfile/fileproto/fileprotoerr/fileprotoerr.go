207553f63849deace84ef7517b92deae58239f96
Sergey Cherepanov <cheggaaa@gmail.com>
2023-03-21 11:23:52 +0100

    update fileproto

--- commonfile/fileproto/fileprotoerr/fileprotoerr.go
+++ commonfile/fileproto/fileprotoerr/fileprotoerr.go
@@ -12,6 +12,6 @@ var (
 	ErrCIDNotFound        = errGroup.Register(fmt.Errorf("CID not found"), uint64(fileproto.ErrCodes_CIDNotFound))
 	ErrForbidden          = errGroup.Register(fmt.Errorf("forbidden"), uint64(fileproto.ErrCodes_Forbidden))
 	ErrSpaceLimitExceeded = errGroup.Register(fmt.Errorf("space limit exceeded"), uint64(fileproto.ErrCodes_SpaceLimitExceeded))
-	ErrBlockSizeExceeded  = errGroup.Register(fmt.Errorf("block size exceeded"), uint64(fileproto.ErrCodes_BlockSizeExceeded))
+	ErrQuerySizeExceeded  = errGroup.Register(fmt.Errorf("query size exceeded"), uint64(fileproto.ErrCodes_QuerySizeExceeded))
 	ErrWrongHash          = errGroup.Register(fmt.Errorf("wrong block hash"), uint64(fileproto.ErrCodes_WrongHash))
 )
