2f2100645722e90e5da031db2ae134b5e898ecb1
Sergey Cherepanov <cheggaaa@gmail.com>
2023-03-21 11:10:50 +0100

    update fileproto

--- commonfile/fileproto/fileprotoerr/fileprotoerr.go
+++ commonfile/fileproto/fileprotoerr/fileprotoerr.go
@@ -7,8 +7,10 @@ import (
 )
 
 var (
-	errGroup       = rpcerr.ErrGroup(fileproto.ErrCodes_ErrorOffset)
-	ErrUnexpected  = errGroup.Register(fmt.Errorf("unexpected fileproto error"), uint64(fileproto.ErrCodes_Unexpected))
-	ErrCIDNotFound = errGroup.Register(fmt.Errorf("CID not found"), uint64(fileproto.ErrCodes_CIDNotFound))
-	ErrForbidden   = errGroup.Register(fmt.Errorf("forbidden"), uint64(fileproto.ErrCodes_Forbidden))
+	errGroup            = rpcerr.ErrGroup(fileproto.ErrCodes_ErrorOffset)
+	ErrUnexpected       = errGroup.Register(fmt.Errorf("unexpected fileproto error"), uint64(fileproto.ErrCodes_Unexpected))
+	ErrCIDNotFound      = errGroup.Register(fmt.Errorf("CID not found"), uint64(fileproto.ErrCodes_CIDNotFound))
+	ErrForbidden        = errGroup.Register(fmt.Errorf("forbidden"), uint64(fileproto.ErrCodes_Forbidden))
+	ErrSpaceLimitExceed = errGroup.Register(fmt.Errorf("space limit exceed"), uint64(fileproto.ErrCodes_SpaceLimitExceed))
+	ErrBlockSizeExceed  = errGroup.Register(fmt.Errorf("block size exceed"), uint64(fileproto.ErrCodes_BlockSizeExceed))
 )
