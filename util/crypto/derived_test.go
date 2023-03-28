120306a1df5e85546c22f9106b9c3b9d3ba3d2c8
mcrakhman <rakhmanov.m@gmail.com>
2023-03-28 21:29:12 +0200

    Add some tests

---
+++ util/crypto/derived_test.go
@@ -0,0 +1,22 @@
+package crypto
+
+import (
+	"crypto/rand"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestDerivedKey(t *testing.T) {
+	seed := make([]byte, 32)
+	_, err := rand.Read(seed)
+	require.NoError(t, err)
+	key, err := DeriveSymmetricKey(seed, AnytypeAccountPath)
+	require.NoError(t, err)
+	_, err = rand.Read(seed)
+	require.NoError(t, err)
+	res, err := key.Encrypt(seed)
+	require.NoError(t, err)
+	dec, err := key.Decrypt(res)
+	require.NoError(t, err)
+	require.Equal(t, seed, dec)
+}
--- util/crypto/ed25519_test.go
+++ util/crypto/ed25519_test.go
@@ -9,12 +9,25 @@ import (
 func Test_EncryptDecrypt(t *testing.T) {
 	privKey, pubKey, err := GenerateEd25519Key(rand.Reader)
 	require.NoError(t, err)
-	msg := make([]byte, 32768)
+	msg := make([]byte, 32000)
 	_, err = rand.Read(msg)
 	require.NoError(t, err)
 	enc, err := pubKey.Encrypt(msg)
 	require.NoError(t, err)
 	dec, err := privKey.Decrypt(enc)
 	require.NoError(t, err)
+	require.NotEqual(t, enc, dec)
 	require.Equal(t, dec, msg)
+}
+
+func Test_SignVerify(t *testing.T) {
+	privKey, pubKey, err := GenerateEd25519Key(rand.Reader)
+	require.NoError(t, err)
+	msg := make([]byte, 32000)
+	_, err = rand.Read(msg)
+	sign, err := privKey.Sign(msg)
+	require.NoError(t, err)
+	res, err := pubKey.Verify(msg, sign)
+	require.NoError(t, err)
+	require.True(t, res)
 }
