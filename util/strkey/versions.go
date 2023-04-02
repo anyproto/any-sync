0ebf93e3834222ef5b4c27689bc57193ce172f1d
Sergey Cherepanov <cheggaaa@gmail.com>
2023-04-02 14:34:03 +0200

    strkey: newtwork id encoding

--- util/strkey/versions.go
+++ util/strkey/versions.go
@@ -4,4 +4,5 @@ const (
 	AccountAddressVersionByte VersionByte = 0x5b // Base58-encodes to 'A...'
 	AccountSeedVersionByte    VersionByte = 0xff // Base58-encodes to 'S...'
 	DeviceSeedVersionByte     VersionByte = 0x7d // Base58-encodes to 'D...'
+	NetworkAddressVersionByte VersionByte = 0xd3 // Base58-encodes to `N...`
 )
