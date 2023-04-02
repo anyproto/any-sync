20a4d85836d2d9e694fbd27b912422a594481566
mcrakhman <rakhmanov.m@gmail.com>
2023-04-02 18:13:10 +0200

    Update cryptography and add master key

--- util/crypto/mnemonic_test.go
+++ util/crypto/mnemonic_test.go
@@ -2,6 +2,7 @@ package crypto
 
 import (
 	"crypto/rand"
+	"github.com/anytypeio/go-slip10"
 	"github.com/stretchr/testify/require"
 	"strings"
 	"testing"
@@ -12,14 +13,31 @@ func TestMnemonic(t *testing.T) {
 	require.NoError(t, err)
 	parts := strings.Split(string(phrase), " ")
 	require.Equal(t, 12, len(parts))
-	key, err := phrase.DeriveEd25519Key(0)
+	res, err := phrase.DeriveKeys(0)
 	require.NoError(t, err)
 	bytes := make([]byte, 64)
 	_, err = rand.Read(bytes)
 	require.NoError(t, err)
-	sign, err := key.Sign(bytes)
+
+	// testing signing with keys
+	for _, k := range []PrivKey{res.MasterKey, res.Identity, res.OldAccountKey} {
+		sign, err := k.Sign(bytes)
+		require.NoError(t, err)
+		res, err := k.GetPublic().Verify(bytes, sign)
+		require.NoError(t, err)
+		require.True(t, res)
+	}
+
+	// testing derivation
+	masterKey, err := genKey(res.MasterNode)
 	require.NoError(t, err)
-	res, err := key.GetPublic().Verify(bytes, sign)
+	require.True(t, res.MasterKey.Equals(masterKey))
+	identityNode, err := res.MasterNode.Derive(slip10.FirstHardenedIndex)
 	require.NoError(t, err)
-	require.True(t, res)
+	identityKey, err := genKey(identityNode)
+	require.NoError(t, err)
+	require.True(t, res.Identity.Equals(identityKey))
+	oldAccountRes, err := phrase.deriveForPath(true, 0, anytypeAccountOldPrefix)
+	require.NoError(t, err)
+	require.True(t, res.OldAccountKey.Equals(oldAccountRes.MasterKey))
 }
