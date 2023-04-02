2c73e1947d0063490add3f3ed4ac2fb3703926b7
Sergey Cherepanov <cheggaaa@gmail.com>
2023-04-02 14:34:03 +0200

    strkey: newtwork id encoding

--- util/strkey/strkey_test.go
+++ util/strkey/strkey_test.go
@@ -1,1 +1,19 @@
 package strkey
+
+import (
+	"github.com/anytypeio/any-sync/util/keys/asymmetric/signingkey"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestDecode(t *testing.T) {
+	_, pubKey, err := signingkey.GenerateRandomEd25519KeyPair()
+	require.NoError(t, err)
+	raw, _ := pubKey.Raw()
+	str, err := Encode(NetworkAddressVersionByte, raw)
+	require.NoError(t, err)
+	res, err := Decode(NetworkAddressVersionByte, str)
+	require.NoError(t, err)
+	assert.Equal(t, raw, res)
+}
