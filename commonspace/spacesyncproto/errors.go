9216896777d197290507908a7769e889e8584323
mcrakhman <rakhmanov.m@gmail.com>
2023-04-24 14:12:24 +0200

    Add errors to proto

--- commonspace/spacesyncproto/errors.go
+++ commonspace/spacesyncproto/errors.go
@@ -8,9 +8,11 @@ import (
 var (
 	errGroup = rpcerr.ErrGroup(ErrCodes_ErrorOffset)
 
-	ErrUnexpected      = errGroup.Register(errors.New("unexpected error"), uint64(ErrCodes_Unexpected))
-	ErrSpaceMissing    = errGroup.Register(errors.New("space is missing"), uint64(ErrCodes_SpaceMissing))
-	ErrSpaceExists     = errGroup.Register(errors.New("space exists"), uint64(ErrCodes_SpaceExists))
-	ErrSpaceNotInCache = errGroup.Register(errors.New("space not in cache"), uint64(ErrCodes_SpaceNotInCache))
-	ErrSpaceIsDeleted  = errGroup.Register(errors.New("space is deleted"), uint64(ErrCodes_SpaceIsDeleted))
+	ErrUnexpected           = errGroup.Register(errors.New("unexpected error"), uint64(ErrCodes_Unexpected))
+	ErrSpaceMissing         = errGroup.Register(errors.New("space is missing"), uint64(ErrCodes_SpaceMissing))
+	ErrSpaceExists          = errGroup.Register(errors.New("space exists"), uint64(ErrCodes_SpaceExists))
+	ErrSpaceNotInCache      = errGroup.Register(errors.New("space not in cache"), uint64(ErrCodes_SpaceNotInCache))
+	ErrSpaceIsDeleted       = errGroup.Register(errors.New("space is deleted"), uint64(ErrCodes_SpaceIsDeleted))
+	ErrPeerIsNotResponsible = errGroup.Register(errors.New("peer is not responsible for space"), uint64(ErrCodes_PeerIsNotResponsible))
+	ErrReceiptInvalid       = errGroup.Register(errors.New("space receipt is not valid"), uint64(ErrCodes_ReceiptIsInvalid))
 )
