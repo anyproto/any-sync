9855eb4484a15f4d1994e40845af0e96053c2b74
Sergey Cherepanov <cheggaaa@gmail.com>
2023-05-04 17:39:23 +0200

    request log

--- metric/log_test.go
+++ metric/log_test.go
@@ -1,1 +1,12 @@
 package metric
+
+import (
+	"context"
+	"github.com/anytypeio/any-sync/app/logger"
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	m := &metric{rpcLog: logger.NewNamed("rpcLog")}
+	m.RequestLog(context.Background(), "rpc")
+}
