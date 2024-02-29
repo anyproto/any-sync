92d3ba6237e0dec6e2d69bd9fc57b852586a2ae1
Sergey Cherepanov <cheggaaa@gmail.com>
2024-02-29 09:15:58 +0100

    move acl service from coordinator

---
+++ acl/acl_test.go
@@ -0,0 +1,136 @@
+package acl
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+
+	"github.com/anyproto/any-sync/app"
+	"github.com/anyproto/any-sync/commonspace/object/accountdata"
+	"github.com/anyproto/any-sync/commonspace/object/acl/list"
+	"github.com/anyproto/any-sync/consensus/consensusclient"
+	"github.com/anyproto/any-sync/consensus/consensusclient/mock_consensusclient"
+	"github.com/anyproto/any-sync/consensus/consensusproto"
+	"github.com/anyproto/any-sync/testutil/accounttest"
+)
+
+var ctx = context.Background()
+
+func TestAclService_AddRecord(t *testing.T) {
+	ownerKeys, err := accountdata.NewRandom()
+	require.NoError(t, err)
+	spaceId := "spaceId"
+	ownerAcl, err := list.NewTestDerivedAcl(spaceId, ownerKeys)
+	require.NoError(t, err)
+	inv, err := ownerAcl.RecordBuilder().BuildInvite()
+	require.NoError(t, err)
+
+	t.Run("success", func(t *testing.T) {
+		fx := newFixture(t)
+		defer fx.finish(t)
+
+		expRes := list.WrapAclRecord(inv.InviteRec)
+		var watcherCh = make(chan consensusclient.Watcher)
+		fx.consCl.EXPECT().Watch(spaceId, gomock.Any()).DoAndReturn(func(spaceId string, w consensusclient.Watcher) error {
+			go func() {
+				w.AddConsensusRecords([]*consensusproto.RawRecordWithId{
+					ownerAcl.Root(),
+				})
+				watcherCh <- w
+			}()
+			return nil
+		})
+
+		fx.consCl.EXPECT().AddRecord(ctx, spaceId, inv.InviteRec).Return(expRes, nil)
+		fx.consCl.EXPECT().UnWatch(spaceId)
+
+		res, err := fx.AddRecord(ctx, spaceId, inv.InviteRec)
+		assert.Equal(t, expRes, res)
+		assert.NoError(t, err)
+
+		w := <-watcherCh
+		w.AddConsensusRecords([]*consensusproto.RawRecordWithId{
+			expRes,
+		})
+	})
+	t.Run("error", func(t *testing.T) {
+		fx := newFixture(t)
+		defer fx.finish(t)
+
+		var testErr = errors.New("test")
+
+		fx.consCl.EXPECT().Watch(spaceId, gomock.Any()).DoAndReturn(func(spaceId string, w consensusclient.Watcher) error {
+			go func() {
+				w.AddConsensusError(testErr)
+			}()
+			return nil
+		})
+		fx.consCl.EXPECT().UnWatch(spaceId)
+
+		res, err := fx.AddRecord(ctx, spaceId, inv.InviteRec)
+		assert.Nil(t, res)
+		assert.EqualError(t, err, testErr.Error())
+	})
+
+}
+
+func TestAclService_RecordsAfter(t *testing.T) {
+	ownerKeys, err := accountdata.NewRandom()
+	require.NoError(t, err)
+	spaceId := "spaceId"
+	ownerAcl, err := list.NewTestDerivedAcl(spaceId, ownerKeys)
+	require.NoError(t, err)
+
+	fx := newFixture(t)
+	defer fx.finish(t)
+
+	fx.consCl.EXPECT().Watch(spaceId, gomock.Any()).DoAndReturn(func(spaceId string, w consensusclient.Watcher) error {
+		go func() {
+			w.AddConsensusRecords([]*consensusproto.RawRecordWithId{
+				ownerAcl.Root(),
+			})
+		}()
+		return nil
+	})
+	fx.consCl.EXPECT().UnWatch(spaceId)
+
+	res, err := fx.RecordsAfter(ctx, spaceId, "")
+	require.NoError(t, err)
+	assert.Len(t, res, 1)
+}
+
+func newFixture(t *testing.T) *fixture {
+	ctrl := gomock.NewController(t)
+	fx := &fixture{
+		a:      new(app.App),
+		ctrl:   ctrl,
+		consCl: mock_consensusclient.NewMockService(ctrl),
+		Acl:    New(),
+	}
+
+	fx.consCl.EXPECT().Name().Return(consensusclient.CName).AnyTimes()
+	fx.consCl.EXPECT().Init(gomock.Any()).AnyTimes()
+	fx.consCl.EXPECT().Run(gomock.Any()).AnyTimes()
+	fx.consCl.EXPECT().Close(gomock.Any()).AnyTimes()
+
+	fx.a.Register(fx.consCl).Register(fx.Acl).Register(&accounttest.AccountTestService{})
+
+	require.NoError(t, fx.a.Start(ctx))
+	return fx
+}
+
+type fixture struct {
+	a      *app.App
+	ctrl   *gomock.Controller
+	consCl *mock_consensusclient.MockService
+	Acl
+}
+
+func (fx *fixture) finish(t *testing.T) {
+	require.NoError(t, fx.a.Close(ctx))
+	fx.ctrl.Finish()
+}
---
+++ acl/object.go
@@ -0,0 +1,94 @@
+package acl
+
+import (
+	"context"
+	"slices"
+	"sync"
+	"time"
+
+	"github.com/anyproto/any-sync/commonspace/object/acl/list"
+	"github.com/anyproto/any-sync/commonspace/object/acl/liststorage"
+	"github.com/anyproto/any-sync/consensus/consensusproto"
+	"go.uber.org/atomic"
+	"go.uber.org/zap"
+)
+
+func (as *aclService) newAclObject(ctx context.Context, id string) (*aclObject, error) {
+	obj := &aclObject{
+		id:         id,
+		aclService: as,
+		ready:      make(chan struct{}),
+	}
+	if err := as.consService.Watch(id, obj); err != nil {
+		return nil, err
+	}
+	select {
+	case <-obj.ready:
+		if obj.consErr != nil {
+			_ = as.consService.UnWatch(id)
+			return nil, obj.consErr
+		}
+		return obj, nil
+	case <-ctx.Done():
+		_ = as.consService.UnWatch(id)
+		return nil, ctx.Err()
+	}
+}
+
+type aclObject struct {
+	id         string
+	aclService *aclService
+	store      liststorage.ListStorage
+
+	list.AclList
+	ready   chan struct{}
+	consErr error
+
+	lastUsage atomic.Time
+
+	mu sync.Mutex
+}
+
+func (a *aclObject) AddConsensusRecords(recs []*consensusproto.RawRecordWithId) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	slices.Reverse(recs)
+	if a.store == nil {
+		defer close(a.ready)
+		if a.store, a.consErr = liststorage.NewInMemoryAclListStorage(a.id, recs); a.consErr != nil {
+			return
+		}
+		if a.AclList, a.consErr = list.BuildAclListWithIdentity(a.aclService.accountService.Account(), a.store, list.NoOpAcceptorVerifier{}); a.consErr != nil {
+			return
+		}
+	} else {
+		a.Lock()
+		defer a.Unlock()
+		if err := a.AddRawRecords(recs); err != nil {
+			log.Warn("unable to add consensus records", zap.Error(err), zap.String("spaceId", a.id))
+			return
+		}
+	}
+}
+
+func (a *aclObject) AddConsensusError(err error) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if a.store == nil {
+		a.consErr = err
+		close(a.ready)
+	} else {
+		log.Warn("got consensus error", zap.Error(err))
+	}
+}
+
+func (a *aclObject) Close() (err error) {
+	return a.aclService.consService.UnWatch(a.id)
+}
+
+func (a *aclObject) TryClose(objectTTL time.Duration) (res bool, err error) {
+	if a.lastUsage.Load().Before(time.Now().Add(-objectTTL)) {
+		return true, a.Close()
+	}
+	return false, nil
+}
