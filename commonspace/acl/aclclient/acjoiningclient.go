81e10fcc5348b468a0000e3b441c6fe6513c60b8
mcrakhman <rakhmanov.m@gmail.com>
2024-02-29 18:46:04 +0100

    Add cancel join for acl client

--- commonspace/acl/aclclient/acjoiningclient.go
+++ commonspace/acl/aclclient/acjoiningclient.go
@@ -18,7 +18,7 @@ const CName = "common.acl.aclclient"
 type AclJoiningClient interface {
 	app.Component
 	AclGetRecords(ctx context.Context, spaceId, aclHead string) ([]*consensusproto.RawRecordWithId, error)
-	RequestJoin(ctx context.Context, spaceId string, payload list.RequestJoinPayload) error
+	RequestJoin(ctx context.Context, spaceId string, payload list.RequestJoinPayload) (aclHeadId string, err error)
 	CancelRemoveSelf(ctx context.Context, spaceId string) (err error)
 }
 
@@ -45,37 +45,42 @@ func (c *aclJoiningClient) AclGetRecords(ctx context.Context, spaceId, aclHead s
 	return c.coordinatorClient.AclGetRecords(ctx, spaceId, aclHead)
 }
 
-func (c *aclJoiningClient) RequestJoin(ctx context.Context, spaceId string, payload list.RequestJoinPayload) (err error) {
+func (c *aclJoiningClient) RequestJoin(ctx context.Context, spaceId string, payload list.RequestJoinPayload) (aclHeadId string, err error) {
 	res, err := c.AclGetRecords(ctx, spaceId, "")
 	if err != nil {
-		return err
+		return
 	}
 	if len(res) == 0 {
-		return fmt.Errorf("acl not found")
+		err = fmt.Errorf("acl not found")
+		return
 	}
 	storage, err := liststorage.NewInMemoryAclListStorage(res[0].Id, res)
 	if err != nil {
-		return err
+		return
 	}
 	acl, err := list.BuildAclListWithIdentity(c.keys, storage, list.NoOpAcceptorVerifier{})
 	if err != nil {
-		return err
+		return
 	}
 	joinRecs, err := acl.AclState().JoinRecords(false)
 	if err != nil {
-		return err
+		return
 	}
 	for _, rec := range joinRecs {
 		if rec.RequestIdentity.Equals(c.keys.SignKey.GetPublic()) {
 			// that means that we already requested to join
-			return nil
+			return
 		}
 	}
 	rec, err := acl.RecordBuilder().BuildRequestJoin(payload)
 	if err != nil {
 		return
 	}
-	_, err = c.coordinatorClient.AclAddRecord(ctx, spaceId, rec)
+	recWithId, err := c.coordinatorClient.AclAddRecord(ctx, spaceId, rec)
+	if err != nil {
+		return
+	}
+	aclHeadId = recWithId.Id
 	return
 }
 
