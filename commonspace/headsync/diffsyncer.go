326371c7b5a35d22e59b3653dc8425c539faa744
mcrakhman <rakhmanov.m@gmail.com>
2024-07-30 23:22:10 +0200

    Use peer context

--- commonspace/headsync/diffsyncer.go
+++ commonspace/headsync/diffsyncer.go
@@ -100,7 +100,7 @@ func (d *diffSyncer) Sync(ctx context.Context) error {
 	}
 	d.log.DebugCtx(ctx, "start diffsync", zap.Strings("peerIds", peerIds))
 	for _, p := range peers {
-		if err = d.syncWithPeer(peer.CtxWithPeerId(ctx, p.Id()), p); err != nil {
+		if err = d.syncWithPeer(p.Context(), p); err != nil {
 			d.log.ErrorCtx(ctx, "can't sync with peer", zap.String("peer", p.Id()), zap.Error(err))
 		}
 	}
