49b9964b1317e0eed405824077f4519f2af18533
AnastasiaShemyakinskaya <shem98a@mail.ru>
2024-08-19 15:23:28 +0200

    GO-3273: refactoring
    Signed-off-by: AnastasiaShemyakinskaya <shem98a@mail.ru>

--- commonspace/object/tree/objecttree/historytree.go
+++ commonspace/object/tree/objecttree/historytree.go
@@ -28,24 +28,47 @@ func (h *historyTree) rebuildFromStorage(params HistoryTreeParams) (err error) {
 
 func (h *historyTree) rebuild(params HistoryTreeParams) (err error) {
 	h.treeBuilder.Reset()
+
 	if len(params.Heads) == 0 {
 		h.tree, err = h.treeBuilder.BuildFull()
-		return
+		return err
 	}
 
-	if len(params.Heads) == 1 && params.Heads[0] == "" {
-		params.Heads, err = h.treeStorage.Heads()
-		if err != nil {
-			return
-		}
+	if len(params.Heads) == 1 {
+		return h.rebuildWithSingleHead(params, params.Heads[0])
 	}
-	if !params.IncludeBeforeId && len(params.Heads) == 1 {
-		beforeChange, err := h.treeBuilder.loadChange(params.Heads[0])
-		if err != nil {
-			return err
-		}
-		params.Heads = beforeChange.PreviousIds
+
+	h.tree, err = h.treeBuilder.build(params.Heads, nil, nil)
+	return err
+}
+
+func (h *historyTree) rebuildWithSingleHead(params HistoryTreeParams, head string) (err error) {
+	if head == "" {
+		return h.rebuildWithEmptyHead(&params)
+	}
+	if !params.IncludeBeforeId {
+		return h.rebuildWithPreviousHead(&params)
 	}
 	h.tree, err = h.treeBuilder.build(params.Heads, nil, nil)
-	return
+	return err
+}
+
+func (h *historyTree) rebuildWithEmptyHead(params *HistoryTreeParams) (err error) {
+	heads, err := h.treeStorage.Heads()
+	if err != nil {
+		return err
+	}
+	params.Heads = heads
+	h.tree, err = h.treeBuilder.build(params.Heads, nil, nil)
+	return err
+}
+
+func (h *historyTree) rebuildWithPreviousHead(params *HistoryTreeParams) (err error) {
+	beforeChange, err := h.treeBuilder.loadChange(params.Heads[0])
+	if err != nil {
+		return err
+	}
+	params.Heads = beforeChange.PreviousIds
+	h.tree, err = h.treeBuilder.build(params.Heads, nil, nil)
+	return err
 }
