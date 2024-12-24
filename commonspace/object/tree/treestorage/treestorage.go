21a2cb82e23ff947f2fe516ab6f629e2de19d021
mcrakhman <rakhmanov.m@gmail.com>
2024-12-24 12:55:13 +0100

    Add some old helpers

--- commonspace/object/tree/treestorage/treestorage.go
+++ commonspace/object/tree/treestorage/treestorage.go
@@ -2,7 +2,9 @@
 package treestorage
 
 import (
+	"bytes"
 	"errors"
+	"strings"
 
 	"github.com/anyproto/any-sync/commonspace/object/tree/treechangeproto"
 )
@@ -15,4 +17,32 @@ type TreeStorageCreatePayload struct {
 	RootRawChange *treechangeproto.RawTreeChangeWithId
 	Changes       []*treechangeproto.RawTreeChangeWithId
 	Heads         []string
+}
+
+func ParseHeads(headsPayload []byte) []string {
+	return strings.Split(string(headsPayload), "/")
+}
+
+func CreateHeadsPayload(heads []string) []byte {
+	return JoinStringsToBytes(heads...)
+}
+
+func JoinStringsToBytes(strs ...string) []byte {
+	var (
+		b        bytes.Buffer
+		totalLen int
+	)
+	for _, s := range strs {
+		totalLen += len(s)
+	}
+	// adding separators
+	totalLen += len(strs) - 1
+	b.Grow(totalLen)
+	for idx, s := range strs {
+		if idx > 0 {
+			b.WriteString("/")
+		}
+		b.WriteString(s)
+	}
+	return b.Bytes()
 }
