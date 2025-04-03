ac26e34ac291c65c4bbf0bfe0fe6992becc0e94f
Anatolii Smolianinov <zarkonesmall@gmail.com>
2025-04-03 17:58:31 +0200

    rm sturct

--- coordinator/inboxclient/messagereceiver_test.go
+++ coordinator/inboxclient/messagereceiver_test.go
@@ -5,11 +5,3 @@ import "github.com/anyproto/any-sync/coordinator/coordinatorproto"
 type MessageReceiverTest interface {
 	Receive(event *coordinatorproto.InboxNotifySubscribeEvent)
 }
-
-type messageReceiverTest struct {
-	mr MessageReceiver
-}
-
-func (f *messageReceiverTest) Receive(event *coordinatorproto.InboxNotifySubscribeEvent) {
-	f.mr(event)
-}
