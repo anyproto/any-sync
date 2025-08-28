23e10ce8d85326646d8f173512bfc619a85f44c7
Roman Khafizianov <requilence@gmail.com>
2025-08-28 20:41:17 +0200

    commonfile: refactor fileservice to allow use it as handler with custom dag service

--- commonfile/fileservice/fileservice.go
+++ commonfile/fileservice/fileservice.go
@@ -2,20 +2,14 @@ package fileservice
 
 import (
 	"context"
-	"fmt"
+	"io"
+
 	"github.com/anyproto/any-sync/app"
 	"github.com/anyproto/any-sync/app/logger"
 	"github.com/anyproto/any-sync/commonfile/fileblockstore"
-	chunker "github.com/ipfs/boxo/chunker"
-	"github.com/ipfs/boxo/ipld/merkledag"
-	"github.com/ipfs/boxo/ipld/unixfs/importer/balanced"
-	"github.com/ipfs/boxo/ipld/unixfs/importer/helpers"
 	ufsio "github.com/ipfs/boxo/ipld/unixfs/io"
 	"github.com/ipfs/go-cid"
 	ipld "github.com/ipfs/go-ipld-format"
-	"github.com/multiformats/go-multihash"
-	"go.uber.org/zap"
-	"io"
 )
 
 const CName = "common.commonfile.fileservice"
@@ -41,68 +35,18 @@ type FileService interface {
 	HasCid(ctx context.Context, c cid.Cid) (exists bool, err error)
 	app.Component
 }
+
+// fileService is a thin wrapper for dependency injection
 type fileService struct {
-	bs        fileblockstore.BlockStoreLocal
-	merkledag ipld.DAGService
-	prefix    cid.Prefix
+	*FileHandler
 }
 
 func (fs *fileService) Init(a *app.App) (err error) {
-	prefix, err := merkledag.PrefixForCidVersion(1)
-	if err != nil {
-		return fmt.Errorf("bad CID Version: %s", err)
-	}
-	hashFunCode, ok := multihash.Names["sha2-256"]
-	if !ok {
-		return fmt.Errorf("unrecognized hash function")
-	}
-	prefix.MhType = hashFunCode
-	prefix.MhLength = -1
-	fs.bs = a.MustComponent(fileblockstore.CName).(fileblockstore.BlockStoreLocal)
-	fs.merkledag = merkledag.NewDAGService(newBlockService(fs.bs))
-	fs.prefix = prefix
-	return
+	bs := a.MustComponent(fileblockstore.CName).(fileblockstore.BlockStoreLocal)
+	fs.FileHandler = NewFileHandler(bs)
+	return nil
 }
 
 func (fs *fileService) Name() string {
 	return CName
-}
-
-func (fs *fileService) DAGService() ipld.DAGService {
-	return fs.merkledag
-}
-
-func (fs *fileService) AddFile(ctx context.Context, r io.Reader) (ipld.Node, error) {
-	dbp := helpers.DagBuilderParams{
-		Dagserv:    fs.merkledag,
-		Maxlinks:   helpers.DefaultLinksPerBlock,
-		CidBuilder: &fs.prefix,
-	}
-	dbh, err := dbp.New(chunker.NewSizeSplitter(r, ChunkSize))
-	if err != nil {
-		return nil, err
-	}
-	n, err := balanced.Layout(dbh)
-	if err != nil {
-		return nil, err
-	}
-	log.Debug("add file", zap.String("cid", n.Cid().String()))
-	return n, nil
-}
-
-func (fs *fileService) GetFile(ctx context.Context, c cid.Cid) (ufsio.ReadSeekCloser, error) {
-	log.Debug("get file", zap.String("cid", c.String()))
-	n, err := fs.merkledag.Get(ctx, c)
-	if err != nil {
-		return nil, err
-	}
-	return ufsio.NewDagReader(ctx, n, fs.merkledag)
-}
-
-func (fs *fileService) HasCid(ctx context.Context, c cid.Cid) (exists bool, err error) {
-	res, err := fs.bs.ExistsCids(ctx, []cid.Cid{c})
-	if err != nil {
-		return
-	}
-	return len(res) > 0, nil
 }
